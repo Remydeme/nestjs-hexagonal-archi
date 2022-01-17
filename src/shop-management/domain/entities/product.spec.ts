f45a5257aa361e601991c3801cb2d11cf811686a
remy deme <remydeme@MacBook-Pro-de-remy.local>
2022-01-17 12:15:29 +0100

    add entities spec file

---
+++ src/shop-management/domain/entities/product.spec.ts
@@ -0,0 +1,9 @@
+describe('Unit test | Entity | produit ', () => {
+  describe('create a new product', () => {
+    it('create a new product', () => {
+      //given
+      //when
+      //then
+    });
+  });
+});
